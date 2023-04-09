c706fa19907b95a4a6801e243f8c56822b77e9d2
Lucas1774 <lucas.115@hotmail.com>
2023-04-09 09:20:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+import time
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(__file__), 'service_account.json')
+SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
+CHANNEL_ID = 'UC-lHJZR3Gqxm24_Vd_AJ5Yw'
+
+def main():
+    if os.path.exists('credentials.json'):
+        credentials = Credentials.from_authorized_user_file('credentials.json', scopes = SCOPES)
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes = SCOPES)
+        credentials = flow.run_local_server(port=0)
+        with open('credentials.json', 'w') as token:
+            token.write(credentials.to_json())
+
+    youtube = build('youtube', 'v3', credentials=credentials)
+    nextPageToken = None
+
+    while True:
+        try:
+            response = youtube.commentThreads().list(
+                part = "id,snippet",
+                allThreadsRelatedToChannelId = CHANNEL_ID,
+                maxResults = 100,
+                textFormat ='plainText',
+                pageToken = nextPageToken
+            ).execute()
+            time.sleep(3)
+        except HttpError as error:
+            print(f'An error occurred: {error}')
+            nextPageToken = None
+            time.sleep(3)
+
+        for item in response['items']:
+            try:
+                comment_id = item['snippet']['topLevelComment']['id']
+                print ("comment ID: " + comment_id)
+                youtube.comments().delete(id = comment_id).execute()
+                time.sleep(3)
+            except HttpError as error:
+                print(f'An error occurred: {error}')
+                time.sleep(3)
+                
+        nextPageToken = response.get('nextPageToken')
+        if not nextPageToken:
+            break
+
+if __name__ == '__main__':
+    main()
